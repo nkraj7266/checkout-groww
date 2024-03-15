f3ab1ad153f994b63fe0c90abda8b2841dc74805
nkraj7266 <nkraj7266@gmail.com>
2024-03-15 12:08:19 +0530

    minor fixes

--- src/app/confirmation/page.jsx
+++ src/app/confirmation/page.jsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import styles from "./confirmation.module.scss";
 import { useSearchParams } from "next/navigation";
 import Image from "next/image";
+import { Suspense } from "react";
 
 import cross from "@/../public/assets/images/cross.png";
 import tick from "@/../public/assets/images/tick.png";
@@ -43,134 +44,152 @@ const Confirmation = () => {
 	}, [data]);
 
 	return (
-		<main className={styles.main}>
-			<div className={styles.container}>
-				<div className={styles.row}>
-					<h1 className={styles.heading}>Order Status</h1>
-					<div className={styles.wrapper}>
-						<div className={styles.status}>
-							<Image
-								src={
-									orderStatus == 1
-										? tick
-										: orderStatus == 2
-										? cross
-										: warn
-								}
-								alt="Order status"
-								width={100}
-								height={100}
-							/>
-							<div className={styles.statusText}>
-								<h2>{`Order ${
-									orderStatus == 1
-										? "Confirmed"
-										: orderStatus == 2
-										? "Failed"
-										: "Pending"
-								}
+		<Suspense fallback={<div>Loading...</div>}>
+			<main className={styles.main}>
+				<div className={styles.container}>
+					<div className={styles.row}>
+						<h1 className={styles.heading}>Order Status</h1>
+						<div className={styles.wrapper}>
+							<div className={styles.status}>
+								<Image
+									src={
+										orderStatus == 1
+											? tick
+											: orderStatus == 2
+											? cross
+											: warn
+									}
+									alt="Order status"
+									width={100}
+									height={100}
+								/>
+								<div className={styles.statusText}>
+									<h2>{`Order ${
+										orderStatus == 1
+											? "Confirmed"
+											: orderStatus == 2
+											? "Failed"
+											: "Pending"
+									}
 									`}</h2>
-								{orderStatus == 1 && (
-									<p>
-										Your order has been confirmed and will
-										be delivered soon.
-									</p>
-								)}
-								{orderStatus == 2 && (
-									<p>
-										Your order has failed. Please try again.
-									</p>
-								)}
-								{orderStatus == 3 && (
-									<p>
-										Please wait ! <br /> You will receive a
-										confirmation email shortly.
-									</p>
-								)}
-							</div>
-						</div>
-					</div>
-					<div className={styles.wrapper}>
-						{orderStatus == 1 && (
-							<div className={styles.orderDetails}>
-								<h2 className={styles.heading}>
-									Order Details
-								</h2>
-								<div className={styles.details}>
-									{payOption && payOption != 3 ? (
-										<div className={styles.paymentDetails}>
-											{payOption == 1 && (
-												<p>
-													Card No. :{" "}
-													<span>123XXXXXXXXXX</span>
-												</p>
-											)}
-											{payOption == 2 && (
-												<p>
-													UPI Ref No. :{" "}
-													<span>123XXXXXXXXXX</span>
-												</p>
-											)}
-											<p>
-												Transaction No. :{" "}
-												<span>
-													T1234-5678-9012-3456
-												</span>
-											</p>
-											<p>
-												Date of Transaction :{" "}
-												<span>
-													{new Date().toLocaleDateString() +
-														" " +
-														new Date().toLocaleTimeString()}
-												</span>
-											</p>
-										</div>
-									) : (
-										<div className={styles.paymentDetails}>
-											<p>
-												Mode of Payment :{" "}
-												<span>Cash on Delivery</span>
-											</p>
-											<p>
-												Delivery Address :{" "}
-												<span>
-													123, XYZ Colony, ABC City,
-													123456
-												</span>
-											</p>
-										</div>
-									)}
-									<div className={styles.amountDetails}>
-										<div>
-											<p>Total Price :</p>
-											<span>Rs. {totalPrice}</span>
-										</div>
-										<div>
-											{payOption == 3 ? (
-												<p>Amount to be Paid :</p>
-											) : (
-												<p>Amount Paid :</p>
-											)}
-											<span>Rs. {finalPrice}</span>
-										</div>
-
-										<p style={{ color: "green" }}>
-											! You{" "}
-											{payOption == 3
-												? "will save"
-												: "saved"}{" "}
-											<strong>Rs. {discount}</strong> on
-											this order
+									{orderStatus == 1 && (
+										<p>
+											Your order has been confirmed and
+											will be delivered soon.
 										</p>
-									</div>
+									)}
+									{orderStatus == 2 && (
+										<p>
+											Your order has failed. Please try
+											again.
+										</p>
+									)}
+									{orderStatus == 3 && (
+										<p>
+											Please wait ! <br /> You will
+											receive a confirmation email
+											shortly.
+										</p>
+									)}
 								</div>
 							</div>
-						)}
+						</div>
+						<div className={styles.wrapper}>
+							{orderStatus == 1 && (
+								<div className={styles.orderDetails}>
+									<h2 className={styles.heading}>
+										Order Details
+									</h2>
+									<div className={styles.details}>
+										{payOption && payOption != 3 ? (
+											<div
+												className={
+													styles.paymentDetails
+												}
+											>
+												{payOption == 1 && (
+													<p>
+														Card No. :{" "}
+														<span>
+															123XXXXXXXXXX
+														</span>
+													</p>
+												)}
+												{payOption == 2 && (
+													<p>
+														UPI Ref No. :{" "}
+														<span>
+															123XXXXXXXXXX
+														</span>
+													</p>
+												)}
+												<p>
+													Transaction No. :{" "}
+													<span>
+														T1234-5678-9012-3456
+													</span>
+												</p>
+												<p>
+													Date of Transaction :{" "}
+													<span>
+														{new Date().toLocaleDateString() +
+															" " +
+															new Date().toLocaleTimeString()}
+													</span>
+												</p>
+											</div>
+										) : (
+											<div
+												className={
+													styles.paymentDetails
+												}
+											>
+												<p>
+													Mode of Payment :{" "}
+													<span>
+														Cash on Delivery
+													</span>
+												</p>
+												<p>
+													Delivery Address :{" "}
+													<span>
+														123, XYZ Colony, ABC
+														City, 123456
+													</span>
+												</p>
+											</div>
+										)}
+										<div className={styles.amountDetails}>
+											<div>
+												<p>Total Price :</p>
+												<span>Rs. {totalPrice}</span>
+											</div>
+											<div>
+												{payOption == 3 ? (
+													<p>Amount to be Paid :</p>
+												) : (
+													<p>Amount Paid :</p>
+												)}
+												<span>Rs. {finalPrice}</span>
+											</div>
+
+											<p style={{ color: "green" }}>
+												! You{" "}
+												{payOption == 3
+													? "will save"
+													: "saved"}{" "}
+												<strong>Rs. {discount}</strong>{" "}
+												on this order
+											</p>
+										</div>
+									</div>
+								</div>
+							)}
+						</div>
 					</div>
 				</div>
-			</div>
-		</main>
+			</main>
+		</Suspense>
 	);
 };
 
